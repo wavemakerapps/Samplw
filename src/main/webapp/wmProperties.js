d04da66fd4569a7018c0ccf5bc04c28b0771dcd3
RKECUYQamx <appTest2@wavemaker.com>
2017-09-16 06:58:19 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Samplw",
+  "homePage" : "Main",
+  "name" : "Samplw",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
